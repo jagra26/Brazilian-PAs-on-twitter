19ac97f93fbaf75f5b274649bce0b6a8594726bd
jagra26 <jagra@ic.ufal.br>
2021-10-05 14:08:48 -0300

    new uc

--- getUC.py
+++ getUC.py
@@ -8,13 +8,13 @@ import re
 from alive_progress import alive_bar
 
 
-def getUC(file, name, listUC):
+def getUC(file, name, keylist, ucComplete):
 	df = pd.read_csv(file, lineterminator='\n')
 	db = {}
 	with alive_bar(len(df.index)) as bar:
 		for tweet in df.index:
-			for UC in listUC:
-				if UC.casefold() in str(df['text'][tweet]).casefold():
+			for UC in keylist:
+				if " "+UC.casefold() in str(df['text'][tweet]).casefold():
 					try:
 						'''wordsUC = UC.casefold().split(" ")
 						wordsTweet = re.sub(r'[^\w\s]', ' ', df['text'][tweet].casefold()).split(" ")
@@ -25,7 +25,8 @@ def getUC(file, name, listUC):
 								location += " "
 								location += wordsTweet[index + i]
 						#print(location)'''
-						location = UC
+						index = keylist.index(UC)
+						location = ucComplete[index]
 						db.update({df['id'][tweet] : location})
 						bar()
 						break
@@ -34,7 +35,7 @@ def getUC(file, name, listUC):
 	UCs = pd.DataFrame.from_dict(db, orient='index')
 	UCs.to_csv(name+".csv")
 
-listUC = ["Unidade de conservação" , "Area protegida" , 
+'''listUC = ["Unidade de conservação" , "Area protegida" , 
 "Parque Nacional" , "Parque estadual" , "Parque natural municipal" , 
 "Parque municipal" , "Estação ecológica" , "Reserva biológica" , 
 "Monumento natural" , "Refúgio da vida silvestre" , "Reserva extrativista" , 
@@ -42,17 +43,15 @@ listUC = ["Unidade de conservação" , "Area protegida" ,
 "Floresta municipal" , "Reserva de desenvolvimento sustentável" , 
 "Área de relevante interesse", "Reserva Particular do Patrimônio Natural"]
 allUCs = pd.read_csv("cnuc_2020_2-semestre.csv", error_bad_lines=False, sep=';')
-allUCsList = allUCs['Nome da UC'].tolist()
+allUCsList = allUCs['Nome da UC'].tolist()'''
+keyword = pd.read_csv("LISTAS-DE-UCS_NOME-PROPRIO.csv", error_bad_lines=False, sep=',')
+keywordList = keyword['palavra chave'].tolist()
+UCList = keyword['nome da uc'].tolist()
+
 for directory in glob.glob(var.path + "2*"):
     print(directory)
     if len(glob.glob(directory + "/*_limpo*.csv")) != 0:
 	    file = glob.glob(directory + "/*_limpo*.csv")[0]
-	    getUC(file, directory + "/clearUC", allUCsList)
+	    getUC(file, directory + "/clearkeyUC", keywordList, UCList)
 	
 
-"""tweet = "Acabou de publicar uma foto em Parque Nacional da Chapada dos Guimarães https://t.co/CJN2U1ANpG"
-uc = "Parque Nacional"
-if uc in tweet:
-	words = tweet.split(" ")
-	index = words.index("Nacional")
-	print(uc, words[index+1], words[index+2], words[index+3], words[index+4])"""
